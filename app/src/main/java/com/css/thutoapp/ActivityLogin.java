81e528135e5294a711320c47a2f4cdc7c4849618
simphiweharrison <simphiweharrison@gmail.com>
2023-06-12 01:34:42 +0200

    Home Fragment design complete

--- app/src/main/java/com/css/thutoapp/ActivityLogin.java
+++ app/src/main/java/com/css/thutoapp/ActivityLogin.java
@@ -2,7 +2,10 @@ package com.css.thutoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityLogin extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button homefragmentnxt = findViewById(R.id.btngoogle);
+
+        homefragmentnxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vhm) {
+                Intent intent = new Intent(ActivityLogin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
