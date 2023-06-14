7e203791a0942f193ac901f3b97aa3f5ea334ae4
simphiweharrison <simphiweharrison@gmail.com>
2023-06-15 00:09:01 +0200

    Quiz screen, select screen complete

--- app/src/main/java/com/css/thutoapp/CategoryActivity.java
+++ app/src/main/java/com/css/thutoapp/CategoryActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
-        //edittxtmail = findViewById(R.id.edTextEmail);
-        //etxtpassword = findViewById(R.id.edTextPassword);
+        setContentView(R.layout.activity_category);
     }
 }
